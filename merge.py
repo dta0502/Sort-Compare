0ca07a74fcaada59e534f768ceb9acccb74bf50e
dta0502 <dta0502@126.com>
2018-08-13 16:03:26 +0800

    update

--- merge.py
+++ merge.py
@@ -1,37 +1,36 @@
-class mergeSort():
-    def merge(self,a,aux,low,mid,high):
-        for k in range(low,high+1):
-            aux[k] = a[k] #辅助数组
-        i = low
-        j = mid+1
-        for k in range(low,high+1):
-            if i > mid:
-                a[k] = aux[j]
-                j += 1
-            elif j > high:
-                a[k] = aux[i]
-                i += 1
-            elif aux[i]>aux[j]:
-                a[k] = aux[j]
-                j += 1
-            else:
-                a[k] = aux[i]
-                i += 1
-    
-    def sort(self,a,aux,low,high):
-        if high <= low:
-            return
-        mid = (low+high)//2 #保证mid是整数
-        self.sort(a,aux,low,mid)
-        self.sort(a,aux,mid+1,high)
-        self.merge(a,aux,low,mid,high)
+def merge(a,aux,low,mid,high):
+    for k in range(low,high+1):
+        aux[k] = a[k] #辅助数组
+    i = low
+    j = mid+1
+    for k in range(low,high+1):
+        if i > mid:
+            a[k] = aux[j]
+            j += 1
+        elif j > high:
+            a[k] = aux[i]
+            i += 1
+        elif aux[i]>aux[j]:
+            a[k] = aux[j]
+            j += 1
+        else:
+            a[k] = aux[i]
+            i += 1
 
-    def mergeSort(self,a):
-        low = 0
-        high = len(a)-1 #注意：0~len(a)-1
-        aux = [0] * len(a)
-        self.sort(a,aux,low,high)
-        return a
+def sort(a,aux,low,high):
+    if high <= low:
+        return
+    mid = (low+high)//2 #保证mid是整数
+    sort(a,aux,low,mid)
+    sort(a,aux,mid+1,high)
+    merge(a,aux,low,mid,high)
+
+def mergeSort(a):
+    low = 0
+    high = len(a)-1 #注意：0~len(a)-1
+    aux = [0] * len(a)
+    sort(a,aux,low,high)
+    return a
 
 if __name__ == '__main__':
     import random
@@ -41,8 +40,7 @@ if __name__ == '__main__':
     random.shuffle(a) #打乱数组a的顺序
     print("未排序数组：")
     print(a)
-    f = mergeSort()
-    f.mergeSort(a)
+    mergeSort(a)
     print("归并排序后数组：")
     print(a)
 
--- selection.py
+++ selection.py
@@ -1,19 +1,18 @@
-class selectionSort():
-    #辅助函数——交换两个数
-    def exchange(self,a,i,j):
-        temp = a[i]
-        a[i] = a[j]
-        a[j] = temp
-    
-    #选择排序函数(无返回值)
-    def selection(self,a):
-        length = len(a)
-        for i in range(length):
-            minIndex = i
-            for j in range(i+1,length):
-                if a[j] < a[minIndex]:
-                    minIndex = j
-            self.exchange(a,i,minIndex)
+#辅助函数——交换两个数
+def exchange(a,i,j):
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
+
+#选择排序函数(无返回值)
+def selection(a):
+    length = len(a)
+    for i in range(length):
+        minIndex = i
+        for j in range(i+1,length):
+            if a[j] < a[minIndex]:
+                minIndex = j
+        exchange(a,i,minIndex)
 
 if __name__ == '__main__':
     import random
@@ -23,7 +22,6 @@ if __name__ == '__main__':
     random.shuffle(a) #打乱数组a的顺序
     print("未排序数组：")
     print(a)
-    sel = selectionSort()
-    sel.selection(a) 
+    selection(a) 
     print("选择排序后数组： ")
     print(a)
